9acbfe7e6057c116fa01c43c9e46b12804221886
Surya <surya.ramshere@gmail.com>
2016-06-04 14:03:38 +0700

    fixing seeder

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,38 +1,13 @@
 import {Component} from '@angular/core';
-import {Routes, Router, ROUTER_DIRECTIVES} from '@angular/router';
-
-import {Dashboard} from './dashboard.component';
-import {Manage} from './manage.component';
-
-@Routes([
-	{path: '/', component: Dashboard},
-	{path: '/manage', component: Manage}
-])
 
 @Component({
 	selector: 'app',
-	directives: [ROUTER_DIRECTIVES],
 	template: `
-		<nav class="navbar navbar-light bg-faded">
-		  	<ul class="nav navbar-nav">
-		    <li class="nav-item active">
-		      <a class="nav-link" href="#">Dashboard <span class="sr-only">(current)</span></a>
-		    </li>
-		    <li class="nav-item">
-		      <a class="nav-link" href="#">Manage</a>
-		    </li>
-		  	</ul>
-		</nav>
-		<router-outlet></router-outlet>
+		<div>Hallo, Welcome to angular2</div>
 	`
 })
 
-export class App {
-	constructor(private router: Router) {
-
-	}
-
-	ngOnInit() {
-		this.router.navigate(['/']);
-	}
+export class App{
+	
+	
 }
--- app/main.ts
+++ app/main.ts
@@ -1,7 +1,5 @@
 import {bootstrap} from '@angular/platform-browser-dynamic';
 import {App} from './app.component';
-import {ROUTER_PROVIDERS, Router} from '@angular/router';
-import {APP_BASE_HREF} from '@angular/common';
-import {provide} from '@angular/core';
 
-bootstrap(App, [ROUTER_PROVIDERS, provide(APP_BASE_HREF, {useValue: '/'})]);
+bootstrap(App);
+
